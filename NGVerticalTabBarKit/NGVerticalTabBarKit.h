09281a8167f55033f7e81d5166356e97c589175b
Juergen Falb <juergen@falb.at>
2017-01-06 22:29:18 +0100

    Added framework to project

---
+++ NGVerticalTabBarKit/NGVerticalTabBarKit.h
@@ -0,0 +1,23 @@
+//
+//  NGVerticalTabBarKit.h
+//  NGVerticalTabBarKit
+//
+//  Created by Juergen Falb on 06/01/2017.
+//  Copyright © 2017 NOUS Wissensmanagement GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NGVerticalTabBarKit.
+FOUNDATION_EXPORT double NGVerticalTabBarKitVersionNumber;
+
+//! Project version string for NGVerticalTabBarKit.
+FOUNDATION_EXPORT const unsigned char NGVerticalTabBarKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NGVerticalTabBarKit/PublicHeader.h>
+#import <NGVerticalTabBarKit/NGVerticalTabBarController.h>
+#import <NGVerticalTabBarKit/NGVerticalTabBarControllerDelegate.h>
+#import <NGVerticalTabBarKit/NGVerticalTabBar.h>
+#import <NGVerticalTabBarKit/NGVerticalTabBarCell.h>
+#import <NGVerticalTabBarKit/NGVerticalTabBarControllerAnimation.h>
+
